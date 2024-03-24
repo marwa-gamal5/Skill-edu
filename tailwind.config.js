ac74be64a9a22d1d4a109adf256b47a439db292e
marwa-gamal5 <marwagamal443@gmail.com>
2024-03-24 15:02:52 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'darkMidnightBlue':'#02305F',
+        'navyBlue':'#235384',
+        'skyBlue':'#58AEDD',
+        'babyBlue':'#B8E1F3',
+        'baleBlue':'#DEF1FF',
+        'whiteSmoke':'#F9F9F9',
+
+
+
+
+
+
+      }
+    },
+  },
+  plugins: [],
+}
+
